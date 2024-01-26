ce682e7b6983e973a42f9e832511424849d6d7e8
Jón Bjarni Bjarnason <jbbjarnason@gmail.com>
2024-01-26 10:34:56 +0000

    systemd support component install

--- tfc_systemd.cmake
+++ tfc_systemd.cmake
@@ -4,9 +4,11 @@ function(tfc_systemd_service_file EXE_TARGET DESCRIPTION)
 
   configure_file("${CMAKE_CURRENT_FUNCTION_LIST_DIR}/systemd/tfc@.service" "${CMAKE_BINARY_DIR}/systemd/${EXE_TARGET}@.service")
 
+  include(GNUInstallDirs)
   install(
     FILES "${CMAKE_BINARY_DIR}/systemd/${EXE_TARGET}@.service"
-    DESTINATION /usr/lib/systemd/system/
+    DESTINATION "${CMAKE_INSTALL_LIBDIR}/systemd/system/"
+    COMPONENT ${EXE_TARGET}
     CONFIGURATIONS Release
   )
 endfunction()
