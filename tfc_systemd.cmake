d8ae81aed100e7690e504650a270abd55e1faa24
Jón Bjarni Bjarnason <jbbjarnason@gmail.com>
2024-01-29 13:33:50 +0000

    systemd supporting component in install

--- tfc_systemd.cmake
+++ tfc_systemd.cmake
@@ -1,4 +1,4 @@
-function(tfc_systemd_service_file EXE_TARGET DESCRIPTION)
+function(tfc_systemd_service_file_in_component EXE_TARGET COMPONENT DESCRIPTION)
   set(EXE_NAME ${EXE_TARGET})
   set(INSTALL_DIR ${CMAKE_INSTALL_BINDIR})
 
@@ -8,7 +8,11 @@ function(tfc_systemd_service_file EXE_TARGET DESCRIPTION)
   install(
     FILES "${CMAKE_BINARY_DIR}/systemd/${EXE_TARGET}@.service"
     DESTINATION "${CMAKE_INSTALL_LIBDIR}/systemd/system/"
-    COMPONENT ${EXE_TARGET}
+    COMPONENT ${COMPONENT}
     CONFIGURATIONS Release
   )
+endfunction()
+
+function(tfc_systemd_service_file EXE_TARGET DESCRIPTION)
+  tfc_systemd_service_file_in_component(${EXE_TARGET} ${EXE_TARGET} ${DESCRIPTION})
 endfunction()
