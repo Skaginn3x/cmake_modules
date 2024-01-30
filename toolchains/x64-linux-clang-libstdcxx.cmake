6086bffc597fcd329f9c8263fc9d95446f15a665
Ómar Högni Guðmarsson <ohg@skaginn3x.com>
2024-01-30 12:06:06 +0000

    Update

--- toolchains/x64-linux-clang-libstdcxx.cmake
+++
@@ -1,8 +0,0 @@
-set(CMAKE_SYSTEM_PROCESSOR x64)
-set(CMAKE_C_COMPILER /cpproot/bin/clang)
-set(CMAKE_CXX_COMPILER /cpproot/bin/clang++)
-set(CMAKE_CXX_FLAGS -stdlib=libstdc++) # TODO this needs to reference the libc++ headers from cpproot
-# set(CMAKE_CXX_FLAGS "-stdlib=libc++ -I/cpproot/include/ -I/cpproot/include/x86_64-unknown-linux-gnu/c++/v1")
-set(CMAKE_C_FLAGS "")
-set(LINK_FLAGS -fuse-ld=lld)
-set(STATIC_LIBRARY_FLAGS -fuse-ld=lld)
--- toolchains/x64-linux-clang.cmake
+++ toolchains/x64-linux-clang.cmake
@@ -2,9 +2,15 @@ set(CMAKE_SYSTEM_PROCESSOR x64)
 set(CMAKE_C_COMPILER /cpproot/bin/clang)
 set(CMAKE_CXX_COMPILER /cpproot/bin/clang++)
 set(CMAKE_CXX_FLAGS "-stdlib=libc++ -I/cpproot/include/ -I/cpproot/include/x86_64-unknown-linux-gnu/c++/v1")
+set(CMAKE_C_FLAGS "-stdlib=libc++ -I/cpproot/include/ -I/cpproot/include/x86_64-unknown-linux-gnu/c++/v1")
 
 set(ENV{CC} /cpproot/bin/clang)
 set(ENV{CXX} /cpproot/bin/clang++)
 set(ENV{CXXFLAGS} "-stdlib=libc++ -I/cpproot/include/ -I/cpproot/include/x86_64-unknown-linux-gnu/c++/v1")
+set(ENV{CFLAGS} "-stdlib=libc++ -I/cpproot/include/ -I/cpproot/include/x86_64-unknown-linux-gnu/c++/v1")
+
 set(CMAKE_EXE_LINKER_FLAGS "-fuse-ld=lld -stdlib=libc++")
 set(ENV{LDFLAGS} "-fuse-ld=lld -stdlib=libc++")
+
+# Specify search paths for libraries
+set(ENV{LIBRARY_PATH} "/cpproot/lib:/cpproot/lib/x86_64-unknown-linux-gnu:$ENV{LIBRARY_PATH}")
