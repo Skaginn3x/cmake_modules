20eb5385c4499594dfb09e975c3115b8dfbaccde
Ómar Högni Guðmarsson <ohg@skaginn3x.com>
2024-01-30 10:13:11 +0000

    Toolchain files updated

--- toolchains/x64-linux-clang-libstdcxx.cmake
+++ toolchains/x64-linux-clang-libstdcxx.cmake
@@ -1,7 +1,8 @@
 set(CMAKE_SYSTEM_PROCESSOR x64)
-set(CMAKE_C_COMPILER clang)
-set(CMAKE_CXX_COMPILER clang++)
-set(CMAKE_CXX_FLAGS -stdlib=libstdc++)
+set(CMAKE_C_COMPILER /cpproot/bin/clang)
+set(CMAKE_CXX_COMPILER /cpproot/bin/clang++)
+set(CMAKE_CXX_FLAGS -stdlib=libstdc++) # TODO this needs to reference the libc++ headers from cpproot
+# set(CMAKE_CXX_FLAGS "-stdlib=libc++ -I/cpproot/include/ -I/cpproot/include/x86_64-unknown-linux-gnu/c++/v1")
 set(CMAKE_C_FLAGS "")
 set(LINK_FLAGS -fuse-ld=lld)
 set(STATIC_LIBRARY_FLAGS -fuse-ld=lld)
--- toolchains/x64-linux-gcc.cmake
+++ toolchains/x64-linux-gcc.cmake
@@ -1,8 +1,9 @@
+#TODO this needs to reference the standard library from cpproot
 set(CMAKE_SYSTEM_PROCESSOR x64)
-set(CMAKE_C_COMPILER gcc)
-set(CMAKE_CXX_COMPILER g++)
+set(CMAKE_C_COMPILER /cpproot/bin/gcc)
+set(CMAKE_CXX_COMPILER /cpproot/bin/g++)
 
-set(ENV{CC} gcc)
-set(ENV{CXX} g++)
+set(ENV{CC} /cpproot/bin/gcc)
+set(ENV{CXX} /cpproot/bin/g++)
 
 # set(CMAKE_EXE_LINKER_FLAGS "-fuse-ld=mold")
