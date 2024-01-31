9fb788699001a23065219752cdc5393a71dc5d4a
Ómar Högni Guðmarsson <ohg@skaginn3x.com>
2024-01-31 16:50:14 +0000

    Toolchain migration (#2)
    * move to cpproot
    * Toolchain files updated
    * Remove c++ standard lib from linker flags

--- toolchains/x64-linux-gcc.cmake
+++ toolchains/x64-linux-gcc.cmake
@@ -1,17 +1,9 @@
+#TODO this needs to reference the standard library from cpproot
 set(CMAKE_SYSTEM_PROCESSOR x64)
-find_program(GCC-13 "gcc-13")
-if(GCC-13)
-  set(CMAKE_C_COMPILER gcc-13)
-  set(CMAKE_CXX_COMPILER g++-13)
+set(CMAKE_C_COMPILER /cpproot/bin/gcc)
+set(CMAKE_CXX_COMPILER /cpproot/bin/g++)
 
-  set(ENV{CC} gcc-13)
-  set(ENV{CXX} g++-13)
-else()
-  set(CMAKE_C_COMPILER gcc)
-  set(CMAKE_CXX_COMPILER g++)
-
-  set(ENV{CC} gcc)
-  set(ENV{CXX} g++)
-endif()
+set(ENV{CC} /cpproot/bin/gcc)
+set(ENV{CXX} /cpproot/bin/g++)
 
 # set(CMAKE_EXE_LINKER_FLAGS "-fuse-ld=mold")
