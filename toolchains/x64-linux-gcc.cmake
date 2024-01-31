9313d8dc2a6641e37e656c7d6954cb20b0094706
Jón Bjarni Bjarnason <jbbjarnason@gmail.com>
2024-01-31 19:29:58 +0000

    if gcc exists in cpproot use it

--- toolchains/x64-linux-gcc.cmake
+++ toolchains/x64-linux-gcc.cmake
@@ -1,9 +1,11 @@
 #TODO this needs to reference the standard library from cpproot
 set(CMAKE_SYSTEM_PROCESSOR x64)
-set(CMAKE_C_COMPILER /cpproot/bin/gcc)
-set(CMAKE_CXX_COMPILER /cpproot/bin/g++)
 
-set(ENV{CC} /cpproot/bin/gcc)
-set(ENV{CXX} /cpproot/bin/g++)
+if(EXISTS "/cpproot/bin/gcc")
+  set(CMAKE_C_COMPILER /cpproot/bin/gcc)
+  set(CMAKE_CXX_COMPILER /cpproot/bin/g++)
+  set(ENV{CC} /cpproot/bin/gcc)
+  set(ENV{CXX} /cpproot/bin/g++)
+endif()
 
 # set(CMAKE_EXE_LINKER_FLAGS "-fuse-ld=mold")
