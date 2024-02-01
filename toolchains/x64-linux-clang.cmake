71b7459e58cb19d004eae85ebd201283aa3a9f9b
Jón Bjarni <jbbjarnason@gmail.com>
2024-02-01 12:12:17 +0000

    Fallback to path (#3)
    * if gcc exists in cpproot use it
    
    * falling back to path and setting environment for gcc to cpproot
    
    * comment sysroot, need to compile libc

--- toolchains/x64-linux-clang.cmake
+++ toolchains/x64-linux-clang.cmake
@@ -1,16 +1,36 @@
 set(CMAKE_SYSTEM_PROCESSOR x64)
-set(CMAKE_C_COMPILER /cpproot/bin/clang)
-set(CMAKE_CXX_COMPILER /cpproot/bin/clang++)
-set(CMAKE_CXX_FLAGS "-stdlib=libc++ -I/cpproot/include/ -I/cpproot/include/x86_64-unknown-linux-gnu/c++/v1")
-set(CMAKE_C_FLAGS "-I/cpproot/include/ -I/cpproot/include/x86_64-unknown-linux-gnu/c++/v1")
+set(COMPILER_MINIMUM_VERSION 17)
 
-set(ENV{CC} /cpproot/bin/clang)
-set(ENV{CXX} /cpproot/bin/clang++)
-set(ENV{CXXFLAGS} "-stdlib=libc++ -I/cpproot/include/ -I/cpproot/include/x86_64-unknown-linux-gnu/c++/v1")
-set(ENV{CFLAGS} "-I/cpproot/include/ -I/cpproot/include/x86_64-unknown-linux-gnu/c++/v1")
+if (EXISTS "/cpproot/bin/clang")
+#  TODO compile libc in container
+#  set(CMAKE_SYSROOT /cpproot)
+  set(CMAKE_C_COMPILER /cpproot/bin/clang)
+  set(CMAKE_CXX_COMPILER /cpproot/bin/clang++)
+  set(CMAKE_CXX_FLAGS "-stdlib=libc++ -I/cpproot/include/ -I/cpproot/include/x86_64-unknown-linux-gnu/c++/v1")
+  set(CMAKE_C_FLAGS "-I/cpproot/include/ -I/cpproot/include/x86_64-unknown-linux-gnu/c++/v1")
 
+  set(ENV{CC} /cpproot/bin/clang)
+  set(ENV{CXX} /cpproot/bin/clang++)
+  set(ENV{CXXFLAGS} "-stdlib=libc++ -I/cpproot/include/ -I/cpproot/include/x86_64-unknown-linux-gnu/c++/v1")
+  set(ENV{CFLAGS} "-I/cpproot/include/ -I/cpproot/include/x86_64-unknown-linux-gnu/c++/v1")
+
+  # Specify search paths for libraries
+  set(ENV{LIBRARY_PATH} "/cpproot/lib:/cpproot/lib/x86_64-unknown-linux-gnu:$ENV{LIBRARY_PATH}")
+else ()
+  set(CMAKE_C_COMPILER clang)
+  set(CMAKE_CXX_COMPILER clang++)
+  set(ENV{CC} clang)
+  set(ENV{CXX} clang++)
+endif ()
 set(CMAKE_EXE_LINKER_FLAGS "-fuse-ld=lld -stdlib=libc++")
 set(ENV{LDFLAGS} "-fuse-ld=lld -stdlib=libc++")
 
-# Specify search paths for libraries
-set(ENV{LIBRARY_PATH} "/cpproot/lib:/cpproot/lib/x86_64-unknown-linux-gnu:$ENV{LIBRARY_PATH}")
+execute_process(COMMAND ${CMAKE_C_COMPILER} -dumpversion OUTPUT_VARIABLE COMPILER_VERSION)
+
+if (COMPILER_VERSION STREQUAL "")
+  message(FATAL_ERROR "Could not determine Clang version!")
+endif ()
+
+if (COMPILER_VERSION VERSION_LESS COMPILER_MINIMUM_VERSION)
+  message(FATAL_ERROR "Clang version must be at least ${COMPILER_MINIMUM_VERSION}! Found ${COMPILER_VERSION}")
+endif ()
--- toolchains/x64-linux-gcc.cmake
+++ toolchains/x64-linux-gcc.cmake
@@ -1,9 +1,33 @@
-#TODO this needs to reference the standard library from cpproot
 set(CMAKE_SYSTEM_PROCESSOR x64)
-set(CMAKE_C_COMPILER /cpproot/bin/gcc)
-set(CMAKE_CXX_COMPILER /cpproot/bin/g++)
+set(COMPILER_MINIMUM_VERSION 13)
 
-set(ENV{CC} /cpproot/bin/gcc)
-set(ENV{CXX} /cpproot/bin/g++)
+if(EXISTS "/cpproot/bin/gcc")
+  #  TODO compile libc in container
+  #  set(CMAKE_SYSROOT /cpproot)
+  set(CMAKE_C_COMPILER /cpproot/bin/gcc)
+  set(CMAKE_CXX_COMPILER /cpproot/bin/g++)
+  set(ENV{CC} /cpproot/bin/gcc)
+  set(ENV{CXX} /cpproot/bin/g++)
 
+  set(CMAKE_CXX_FLAGS "-I/cpproot/include/")
+  set(CMAKE_C_FLAGS "-I/cpproot/include/")
+  set(ENV{CXXFLAGS} "-I/cpproot/include/")
+  set(ENV{CFLAGS} "-I/cpproot/include/")
+  set(ENV{LIBRARY_PATH} "/cpproot/lib:/cpproot/lib/x86_64-unknown-linux-gnu:$ENV{LIBRARY_PATH}")
+else ()
+  set(CMAKE_C_COMPILER gcc)
+  set(CMAKE_CXX_COMPILER g++)
+  set(ENV{CC} gcc)
+  set(ENV{CXX} g++)
+endif()
+
+execute_process(COMMAND ${CMAKE_C_COMPILER} -dumpversion OUTPUT_VARIABLE COMPILER_VERSION)
+
+if (COMPILER_VERSION STREQUAL "")
+  message(FATAL_ERROR "Could not determine GCC version!")
+endif ()
+
+if (COMPILER_VERSION VERSION_LESS COMPILER_MINIMUM_VERSION)
+  message(FATAL_ERROR "GCC version must be at least ${COMPILER_MINIMUM_VERSION}! Found ${COMPILER_VERSION}")
+endif ()
 # set(CMAKE_EXE_LINKER_FLAGS "-fuse-ld=mold")
