6b8042b2f6577a3edd38b917090c186f6c3751bf
magni-mar <135731785+magni-mar@users.noreply.github.com>
2024-02-27 12:48:25 +0000

    move install dirs

--- tfc_systemd.cmake
+++ tfc_systemd.cmake
@@ -2,9 +2,10 @@ function(tfc_systemd_service_file_in_component EXE_TARGET COMPONENT DESCRIPTION)
   set(EXE_NAME ${EXE_TARGET})
   set(INSTALL_DIR ${CMAKE_INSTALL_BINDIR})
 
+  include(GNUInstallDirs)
+
   configure_file("${CMAKE_CURRENT_FUNCTION_LIST_DIR}/systemd/tfc@.service" "${CMAKE_BINARY_DIR}/systemd/${EXE_TARGET}@.service")
 
-  include(GNUInstallDirs)
   install(
     FILES "${CMAKE_BINARY_DIR}/systemd/${EXE_TARGET}@.service"
     DESTINATION "${CMAKE_INSTALL_LIBDIR}/systemd/system/"
